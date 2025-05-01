f3a4293815547d0b9fe7da29f0e0584dfc522bdb
marcandrelam <marclam2005@outlook.com>
2025-05-01 14:50:49 -0400

    final proj 5-1

---
+++ FinalProject/README.md
@@ -0,0 +1,17 @@
+# 5/1 update
+
+Midterm presentation link:
+https://docs.google.com/presentation/d/1PC0Yw6qmxEWD2yxfgkyaIScADdhmYKhIj3xau9KxY4Y/edit?usp=sharing
+
+Created new blog site using HTML, CSS, and JS with GSAP plugins.
+
+Uses GSAP to create typewriter animation, landing shrink animation, and horizontal scroll animation.
+
+Uses CSS flexbox to organize divs for horizontal scroll.
+
+Uses HTML to structure site.
+
+Things to do for 5/8:
+-adding pages for entries
+-updating images & possibly fonts
+-smooth scroll to make animiations feel less clunky
---
+++ FinalProject/script.js
@@ -0,0 +1,39 @@
+gsap.registerPlugin(ScrollTrigger); 
+gsap.registerPlugin(TextPlugin);
+
+//for opening animation subheading
+
+const typewriter = document.getElementById("typewriter");
+
+gsap.to(typewriter,{
+    duration: 5, 
+    text: {
+        value: "Short Essays and Memories from Marc Andre Lam",
+        oldClass: "start",
+        newClass: "end",
+    },
+});
+
+//scrollTrigger hero animation 
+
+gsap.to(".mask", {
+    scrollTrigger: {
+      trigger: "#scrollOpen",
+      scrub: true,
+      pin: true,
+    },
+    scale: .7,
+    height: 300,
+  });
+
+const entries = gsap.utils.toArray("#horizontal .entries"); 
+
+gsap.to(entries, {
+  xPercent: -100* (entries.length -1),
+  scrollTrigger: {
+    trigger: "#horizontal", 
+    pin: true, 
+    scrub: true,
+    end: "+=5000"
+  }
+})
